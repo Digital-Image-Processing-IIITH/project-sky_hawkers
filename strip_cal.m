53fcf708d86145b958f0a0d6a211afe16f451eb7
starc52 <cstarpowful@gmail.com>
2020-10-30 13:36:45 +0530

    dataset parameters

--- strip_cal.m
+++ strip_cal.m
@@ -1,12 +1,12 @@
 % General code for strip calculation
 % Parameters to be filled in after capturing the input
 
-len = "arm length val" % arm length (measure it from center of rotation to optical center)
-rad = "radius val"  % radius for baseline
+len = 20 % in centimetres arm length (measure it from center of rotation to optical center)
+rad = 4  % in centimetres, as in the paper. radius for baseline
 
 beta = asin(rad/len)
 
-f = "focal length of camera val" % Camera's focal length
+f = 18 % in millimetres Camera's focal length
 dist = tan(beta)*f % distance from the centre in mm
 
 conversion_factor = "conversion between inches and mm"  %if necessary
