93844921b0f7b4b9882b11fef38b3d05c6a6f314
Mrinal Khubchandani <47557950+Laxus-dreyar@users.noreply.github.com>
2020-10-31 22:44:02 +0530

    minor modification

--- strip_cal.m
+++ strip_cal.m
@@ -1,5 +1,4 @@
 % General code for strip calculation
-% Parameters to be filled in after capturing the input
 
 len = 20 % in centimetres arm length (measure it from center of rotation to optical center)
 rad = 4  % in centimetres, as in the paper. radius for baseline
@@ -9,6 +8,6 @@ beta = asin(rad/len)
 f = 18 % in millimetres Cameras focal length
 dist = tan(beta)*f % distance from the centre in mm
 
-conversion_factor = "conversion between inches and mm"  %if necessary
+sensor_size = 36  %in mm
 height = 1080
-dist_mm = dist/conversion_factor*height
+dist_mm = dist/sensor_size*height
