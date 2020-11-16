641520f33ecb33102c1b299a6ffbf66a29042564
starc52 <cstarpowful@gmail.com>
2020-11-16 15:03:26 +0530

    added disparity computation

--- code.m
+++ code.m
@@ -94,3 +94,71 @@ Img_l_eye_inf=Img_1(:,1:end-shift_x,:);
 imwrite(uint8(Img_l_eye_inf),'left_eye_inf.jpg'); 
 imwrite(uint8(Img_r_eye_inf),'right_eye_inf.jpg');
 %imshow(Im_left_eye-uint8(Im_right_eye))
+
+%imshow(Im_left_eye-uint8(Im_right_eye))
+%% DISPARITY COMPUTATION
+
+mx1 = -1;
+mx2 = -1;
+
+[r1,c1,ht]=size(Img_l_eye_inf);
+win=50;
+%r=[100:140:420];
+r = [100,240,380];
+cm=floor(c1/win); %for each column
+pos=0;
+
+%calculating left max and right max
+for i=1:r1
+    for j=1:c1
+        for k=1:ht
+            if Img_l_eye_inf(i,j,k)>mx1
+                mx1 = Img_l_eye_inf(i,j,k);
+            end
+            
+            if Img_r_eye_inf(i,j,k)>mx2
+                mx2 = Img_r_eye_inf(i,j,k);
+            end
+           
+        end
+    end
+end
+
+n_Img_l_eye_inf = double(Img_l_eye_inf)./double(mx1);
+n_Img_r_eye_inf = double(Img_r_eye_inf)./double(mx2);
+
+Im_1_dbl=rgb2gray(n_Img_l_eye_inf);
+Im_2_dbl=rgb2gray(n_Img_r_eye_inf);
+
+%disparity computing here
+for pos=1:length(r)
+    idx=1;
+    for c=1:c1
+        lower_limit = c-win/2;
+        upper_limit = c+win/2;
+
+        if(lower_limit>=1 && upper_limit<=c1)
+            tmp1 = Im_1_dbl(r(pos)-win/2:r(pos)+win/2,lower_limit:upper_limit);
+            tmp2 = Im_2_dbl(r(pos)-win/2:r(pos)+win/2,lower_limit:upper_limit);
+
+            Im_temp=tmp1-tmp2;
+            vr=var(Im_temp(:));
+            std(pos,idx)=sqrt(vr);
+            idx=idx+1;
+        end
+    end
+end
+
+[m,n]=size(std);
+final_max=zeros(n,1);
+
+% selecting the maximum value
+for id=1:n
+    value = std(:,id);
+    temp_val=max(value);
+    final_max(id)=temp_val;
+end
+
+%% Smoothening the signal using a Median Filter
+clear filt_out
+filt_out=medfilt1(final_max,400);
